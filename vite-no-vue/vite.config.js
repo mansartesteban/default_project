6cd621637f398cf40e6d9cbb91a9eb65402b9b3b
Esteban Mansart <esteban.mansart@gmail.com>
2024-10-21 22:13:53 +0200

    Adding base project with vite no vue

---
+++ vite-no-vue/vite.config.js
@@ -0,0 +1,27 @@
+import { defineConfig } from "vite"
+import path from "path"
+
+const root = path.resolve(__dirname)
+
+export default defineConfig({
+    resolve: {
+        alias: {
+            "@": path.join(root, "src"),
+        },
+    },
+    css: {
+        preprocessorOptions: {
+            scss: {
+                api: 'modern-compiler' // or "modern"
+            }
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: ["style-loader", "css-loader", "sass-loader"],
+            },
+        ],
+    },
+})
